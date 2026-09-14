21e347893e4f201a5ba801dfefb17b4d29be041a
VIKASCHAUHAN9258 <chunnuchauhan0001@gmail.com>
2026-09-14 14:52:20 +0530

    Time: 4 ms (83.77%), Space: 44.2 MB (82.03%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,20 +1,24 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length()) return false;
-        // Two arrays for frequency count in both s and t
-        int [] freq_S = new int[26];
-        int [] freq_T = new int[26];
+        if(s.length()!=t.length()){
+            return false;
+        }
+        int[] freq_s=new int[26];
+        int[] freq_t=new int[26];
         for(int i=0; i<s.length(); i++){
-            freq_S[s.charAt(i) - 'a']++;
+            freq_s[s.charAt(i) -'a']++;
         }
            for(int j=0; j<t.length(); j++){
-            freq_T[t.charAt(j) - 'a']++;
+            freq_t[t.charAt(j) -'a']++;
         }
         for(int i=0; i<26; i++){
-            if(freq_S[i]!=freq_T[i]){
+            if(freq_s[i]!=freq_t[i]){
                 return false;
             }
+
         }
         return true;
+
+        
     }
 }
